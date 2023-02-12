cda5b0bec03228b962094047c520906f2fa96591
PabloDinella <pablo.dinella@gmail.com>
2023-02-12 19:07:18 -0300

    initial commit

---
+++ components/VerticalNavigation/VerticalNavigation.tsx
@@ -0,0 +1,19 @@
+import { Box, NavLink } from '@mantine/core';
+import { IconGauge, IconFingerprint, IconBook, IconBookmarks, IconInfoSquare } from '@tabler/icons';
+
+function VerticalNavigation() {
+  return (
+    <Box>
+      <NavLink label="Favoritos" icon={<IconBookmarks size={16} stroke={1.5} />} />
+
+      <NavLink label="Hinários" icon={<IconBook size={16} stroke={1.5} />} childrenOffset={28}>
+        <NavLink label="Hinos e Cânticos" />
+        <NavLink label="Hinos Espirituais" />
+      </NavLink>
+
+      <NavLink label="Sobre" icon={<IconInfoSquare size={16} stroke={1.5} />} />
+    </Box>
+  );
+}
+
+export default VerticalNavigation;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,62 @@
-import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { useState } from 'react';
+import {
+  AppShell as MantineAppShell,
+  Navbar,
+  Header,
+  Footer,
+  Aside,
+  Text,
+  MediaQuery,
+  Burger,
+  useMantineTheme,
+  Container,
+} from '@mantine/core';
+import HymnsList from '../components/HymnsList/HymnsList';
+import VerticalNavigation from '../components/VerticalNavigation/VerticalNavigation';
 
-export default function HomePage() {
+export default function AppShell() {
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
   return (
-    <>
-      <Welcome />
-      <ColorSchemeToggle />
-    </>
+    <MantineAppShell
+      styles={{
+        main: {
+          background: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
+        },
+      }}
+      navbarOffsetBreakpoint="sm"
+      asideOffsetBreakpoint="sm"
+      navbar={
+        <Navbar p="md" hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 200, lg: 300 }}>
+          <VerticalNavigation />
+        </Navbar>
+      }
+      footer={
+        <Footer height={60} p="md">
+          audio player
+        </Footer>
+      }
+      header={
+        <Header height={{ base: 50, md: 70 }} p="md">
+          <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+            <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+              <Burger
+                opened={opened}
+                onClick={() => setOpened((o) => !o)}
+                size="sm"
+                color={theme.colors.gray[6]}
+                mr="xl"
+              />
+            </MediaQuery>
+
+            <Text>Hinários Web</Text>
+          </div>
+        </Header>
+      }
+    >
+      <Container px={0}>
+        <HymnsList />
+      </Container>
+    </MantineAppShell>
   );
 }
