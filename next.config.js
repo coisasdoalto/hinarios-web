1de87cae939a280eb2ba6d441c4593e9b5d785d8
PabloDinella <pablo.dinella@gmail.com>
2023-05-28 18:27:42 -0300

    first try setup pwa

--- next.config.js
+++ next.config.js
@@ -2,10 +2,16 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({
-  reactStrictMode: false,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  experimental: { scrollRestoration: true },
+const withPWA = require('next-pwa')({
+  dest: 'public',
 });
+
+module.exports = withBundleAnalyzer(
+  withPWA({
+    reactStrictMode: false,
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    experimental: { scrollRestoration: true },
+  })
+);
