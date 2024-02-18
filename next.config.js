5274a8aa1b0d1f57b824f0d69d18bebf4477da79
PabloDinella <pablo.dinella@gmail.com>
2024-02-18 15:06:24 -0300

    configure serwist

--- next.config.js
+++ next.config.js
@@ -12,18 +12,25 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 //   })
 // );
 
-const { PHASE_PRODUCTION_BUILD } = require('next/constants');
+const revision = '2';
 
-module.exports = async (phase, { defaultConfig }) => {
-  const config = {
-    ...defaultConfig,
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    reactStrictMode: false,
-    experimental: { scrollRestoration: true },
-    trailingSlash: true,
-  };
+const withSerwist = require('@serwist/next').default({
+  // Note: This is only an example. If you use Pages Router,
+  // use something else that works, such as "service-worker/index.ts".
+  swSrc: './sw.ts',
+  swDest: 'public/sw.js',
+  maximumFileSizeToCacheInBytes: 7355608,
+  additionalPrecacheEntries: [
+    { url: '/sobre/', revision },
+    { url: '/corinhos-e-canticos-de-salvacao/1-Ele-E-Exaltado!/', revision },
+  ],
+});
 
-  return config;
-};
+module.exports = withSerwist({
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  reactStrictMode: false,
+  experimental: { scrollRestoration: true },
+  trailingSlash: true,
+});
