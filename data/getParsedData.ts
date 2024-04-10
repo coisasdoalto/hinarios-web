d58f8a17aad2fadf3369c8500f9632cc5edeea62
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2024-04-10 18:39:08 -0300

    chore: moves hymnsData folder from tmp folder one level up

--- data/getParsedData.ts
+++ data/getParsedData.ts
@@ -2,7 +2,7 @@ import { readFile } from 'fs/promises';
 import path from 'path';
 import { ZodType } from 'zod';
 
-export const joinDataPath = (pathToJoin: string) => path.join('tmp', 'hymnsData', pathToJoin);
+export const joinDataPath = (pathToJoin: string) => path.join('hymnsData', pathToJoin);
 
 const getParsedData = async <T>({ filePath, schema }: { filePath: string; schema: ZodType<T> }) => {
   const file = await readFile(joinDataPath(filePath));
