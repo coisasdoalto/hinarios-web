6e939b6e58b1ffdac4a3e0564ff2cad54276df08
PabloDinella <pablo.dinella@gmail.com>
2024-07-01 01:03:16 -0300

    wip

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-import serwist from '@serwist/next';
+import withSerwistInit from '@serwist/next';
 import { readFileSync } from 'fs';
 import { nanoid } from 'nanoid';
 import { PHASE_PRODUCTION_BUILD } from 'next/constants.js';
@@ -13,16 +13,16 @@ const revision = buildId;
 export default async (phase, { defaultConfig }) => {
   const pathsForPrecache = JSON.parse(readFileSync('./tmp/pathsForPrecache.json'));
 
-  const withSerwist = serwist({
+  const withSerwist = withSerwistInit({
     swSrc: './sw.ts',
     swDest: 'public/sw.js',
-    maximumFileSizeToCacheInBytes: 7355608,
+    // maximumFileSizeToCacheInBytes: 7355608,
     disable: phase !== PHASE_PRODUCTION_BUILD,
     additionalPrecacheEntries: [
       { url: '/', revision },
       { url: '/sobre/', revision },
       { url: '/politica-de-privacidade/', revision },
-      ...pathsForPrecache.map((url) => ({ url, revision })),
+      // ...pathsForPrecache.map((url) => ({ url, revision })),
     ],
   });
 
@@ -39,5 +39,6 @@ export default async (phase, { defaultConfig }) => {
     generateBuildId: () => buildId,
   };
 
+  return nextConfig;
   return withSerwist(nextConfig);
 };
